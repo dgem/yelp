b97e7ee0492de1fad0d6220805c8fa22127983c1
Dan Murphy <dan.murphy@ft.com>
2016-05-31 11:53:28 +0100

    End of #demo-basic-appjs

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack');
+const fs = require('fs');
+const path = require('path'),
+    join = path.join,
+    resolve = path.resolve;
+const getConfig = require('hjs-webpack');
+const NODE_ENV = process.env.NODE_ENV;
+const isDev = NODE_ENV === 'development';
+
+
+var config = getConfig({
+    isDev: isDev,
+    in : join(__dirname, 'src/app.js'),
+    out: join(__dirname, 'dist'),
+    clearBeforeBuild: true
+})
+
+module.exports = config;
